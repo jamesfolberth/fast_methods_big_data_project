91777a41360592e17522404567c8ad272dce6bbd
James Folberth <jamesfolberth@gmail.com>
2016-04-21 15:26:07 -0600

    A few changes to SRHT timing experiments

--- matlab/srht_timing.m
+++ matlab/srht_timing.m
@@ -4,8 +4,9 @@ function [] = srht_timing()
 % Theoretical timing is O(d*log(k)) for length d naturally ordered Hadamard
 % transform and picking k elements.
 
-%experiment_01();
-experiment_02();
+experiment_01();
+%experiment_02();
+%experiment_03(); % I don't really like this.  experiment 01 is clear enough
 
 
 end
@@ -19,7 +20,7 @@ end
 
 function [] = experiment_01()
 % {{{
-m_vec = 10:14;
+m_vec = 11:14;
 num_samples = 10;
 x_2nd_dim = 20;
 
@@ -63,21 +64,24 @@ figure(1);
 clf();
 hold on;
 for i=1:numel(m_vec);
-   m = m_vec(i);
-   avg = mean(naive_times{i},1);
-   stddev = std(naive_times{i},0,1);
-   errorbar(k_vecs{i}, avg, stddev, 'b--');
+   %m = m_vec(i);
+   %avg = mean(naive_times{i},1)
+   %stddev = std(naive_times{i},0,1);
+   %errorbar(k_vecs{i}, avg, stddev, 'b--');
    
+   avg = mean(naive_times{i}(:)); % just plot average time as a constant, per Gunnar's recommendation
+   plot([k_vecs{i}(1) k_vecs{i}(end)], [avg avg], 'g--');
+
    m = m_vec(i);
    avg = mean(srht_times{i},1);
    stddev = std(srht_times{i},0,1);
-   errorbar(k_vecs{i}, avg, stddev, 'g-');
+   errorbar(k_vecs{i}, avg, stddev, 'b-');
 end
 set(gca, 'xscale', 'log');
 xlabel('k - number of subsamples');
 ylabel('runtime (s)');
 title('Runtime scaling of SRHT vs. k (various d)');
-legend('Simple SRHT', 'Fancy SRHT', 'Location', 'SouthEast');
+legend('Simple SRHT', 'Better SRHT', 'Location', 'SouthEast');
 hold off;
 
 % }}}
@@ -132,17 +136,91 @@ hold on;
 for i=1:numel(k_vec);
    avg = mean(naive_times{i},1);
    stddev = std(naive_times{i},0,1);
-   errorbar(pow2(m_vecs{i}), avg, stddev, 'b--');
+   errorbar(pow2(m_vecs{i}), avg, stddev, 'g--');
    
    avg = mean(srht_times{i},1);
    stddev = std(srht_times{i},0,1);
-   errorbar(pow2(m_vecs{i}), avg, stddev, 'g-');
+   errorbar(pow2(m_vecs{i}), avg, stddev, 'b-');
 end
+set(gca, 'xscale', 'log', 'yscale', 'log');
 xlabel('d - length of transform');
 ylabel('runtime (s)');
 title('Runtime scaling of SRHT vs. d (various k)');
-legend('Simple SRHT', 'Fancy SRHT', 'Location', 'SouthEast');
+legend('Simple SRHT', 'Better SRHT', 'Location', 'SouthEast');
 hold off;
 
 % }}}
 end
+
+
+function [] = experiment_03()
+% {{{
+m_vec = 11:14;
+num_samples = 10;
+x_2nd_dim = 20;
+
+% run and collect computation times
+k_vecs = cell(numel(m_vec),1);
+naive_times = cell(numel(m_vec),1);
+srht_times = cell(numel(m_vec),1);
+for i=1:numel(m_vec)
+   m = m_vec(i);
+   d = pow2(m);
+
+   x = randn(d,x_2nd_dim);
+   
+   k_vecs{i} = floor(2.^(linspace(4,m,15))); % build our own logspace
+   naive_times{i} = zeros(num_samples, numel(k_vecs{i}));
+   srht_times{i} = zeros(num_samples, numel(k_vecs{i}));
+
+   for j=1:numel(k_vecs{i})
+      k = k_vecs{i}(j);
+      fprintf(1,'\r m = %d, k = %d                             ', m, k);
+      for n=1:num_samples
+         p = randperm(d,k);
+         %[p,~] = sort(p);
+
+         tic();
+         PHx = srht_naive(x,p);
+         t = toc();
+         naive_times{i}(n,j) = t;
+
+         tic();
+         PhX = srht(x,p);
+         t = toc();
+         srht_times{i}(n,j) = t;
+      end
+   end
+end
+fprintf(1,'\n');
+
+% plot things
+figure(1);
+clf();
+hold on;
+for i=1:numel(m_vec);
+   %m = m_vec(i);
+   %avg = mean(naive_times{i},1)
+   %stddev = std(naive_times{i},0,1);
+   %errorbar(k_vecs{i}, avg, stddev, 'b--');
+   
+   avg = mean(naive_times{i}(:)); % just plot average time as a constant, per Gunnar's recommendation
+   plot([k_vecs{i}(1) k_vecs{i}(end)], [avg avg], 'g--');
+
+   m = m_vec(i);
+   avg = mean(srht_times{i},1);
+   stddev = std(srht_times{i},0,1);
+   errorbar(k_vecs{i}, avg./log10(k_vecs{i}), stddev./log10(k_vecs{i}), 'b-');
+end
+set(gca, 'xscale', 'log');
+xlabel('k - number of subsamples');
+ylabel('runtime (s)');
+title('Runtime scaling of SRHT vs. k (various d)');
+legend('Simple SRHT', 'Better SRHT', 'Location', 'SouthEast');
+hold off;
+
+% }}}
+end
+
+
+
